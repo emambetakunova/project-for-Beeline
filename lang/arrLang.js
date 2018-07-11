4f8c899bfe29f2331604481d83412e55af88f95d
uukeshov <uukeshov@gmail.com>
2018-07-11 18:33:21 +0600

    added lang localization

---
+++ lang/arrLang.js
@@ -0,0 +1,22 @@
+var arrLang = {
+  'en' : {
+    'slogan' : 'Home_en',
+    'about' : 'About_en',
+    'contact' : 'Contact Us_en'
+  },
+  'ru' : {
+    'slogan' : 'Домой',
+    'about' : 'О нас',
+    'contact' : 'Связь'
+  },
+  'kg' : {
+    'slogan' : 'KG',
+    'about' : 'О нас',
+    'contact' : 'Связь'
+  },
+  'uz' : {
+    'slogan' : 'YiGo',
+    'about' : 'О нас',
+    'contact' : 'Связь'
+  }
+};
